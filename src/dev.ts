4545362c84269b1fd2a6ce22b2a5d47e9f33b6fe
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-23 18:42:11 +0800

    提交代码

--- src/dev.ts
+++ src/dev.ts
@@ -1,15 +1,15 @@
 import 'global-agent/bootstrap';
 import fs from 'fs';
-import { bitget, binance } from 'ccxt';
+import { bitget, binance, mexc } from 'ccxt';
 
-const secret = require('../secrets/bitget.json');
+const secret = require('../secrets/mexc.json');
 
 function flog(data: any) {
   fs.writeFileSync('output/a.json', JSON.stringify(data, null, 2));
 }
 
 async function dev() {
-  const ex = new bitget(secret);
+  const ex = new mexc(secret);
   await ex.loadMarkets();
   const balance = await ex.fetchBalance();
   flog(balance);
